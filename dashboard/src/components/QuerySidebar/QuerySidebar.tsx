552a022276e6b39a191e3ec8e7183e89f4706b71
Alejandro Guillen <aguillenv93@gmail.com>
2023-04-25 14:07:27 +0200

    remove modules componenet

--- dashboard/src/components/QuerySidebar/QuerySidebar.tsx
+++ dashboard/src/components/QuerySidebar/QuerySidebar.tsx
@@ -2,14 +2,13 @@ import { Tab as ReactTab } from '@headlessui/react';
 
 import { align, Tab, VBox } from '../common';
 import { useAppContext } from '../providers/AppProvider';
-import Modules from './Modules';
 import Schema from './Schema';
 
 export default function QuerySidebar() {
-  const { functions, allModules, installedModules } = useAppContext();
+  const { functions, installedModules } = useAppContext();
   return (
     <div className='h-50vh w-1/4 font-normal text-xs mr-2 overflow-auto' id='query-sidebar'>
-      <Tab tabs={[{ title: 'Schema' }, { title: 'Modules' }]} defaultIndex={1}>
+      <Tab tabs={[{ title: 'Schema' }]} defaultIndex={0}>
         <VBox
           alignment={align.start}
           customClasses='h-sidebar bg-gray-100/20 dark:bg-gray-800 w-full overflow-auto'
@@ -18,9 +17,6 @@ export default function QuerySidebar() {
             <div className='overflow-auto'>
               <Schema moduleData={installedModules} functionData={functions} />
             </div>
-          </ReactTab.Panel>
-          <ReactTab.Panel className='dark:bg-gray-800'>
-            <Modules modulesInstalledData={installedModules} allModules={allModules} />
           </ReactTab.Panel>
         </VBox>
       </Tab>
