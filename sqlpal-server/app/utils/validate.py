d5ea44a36478931579fa3c64955424232ae51871
Alejandro Guillen <aguillenv93@gmail.com>
2023-05-02 16:29:08 +0200

    pep8 format

--- sqlpal-server/app/utils/validate.py
+++ sqlpal-server/app/utils/validate.py
@@ -1,19 +1,21 @@
 from pglast import ast
 
+
 def validate_select(stmt, columns_by_table_dict):
     # todo: improve this later. for now just check is a valid column in the schema. Then we can check if the column is valid for the table, but for that we would need to dig deeper in the ast
-    columns = [col for table in columns_by_table_dict for col in columns_by_table_dict[table]]
+    columns = [
+        col for table in columns_by_table_dict for col in columns_by_table_dict[table]]
     # validate target list to see if they are valid columns
     if stmt.targetList is None:
-      for f in [f for rt in stmt.targetList for f in rt.val.fields]:
-          if f.sval not in columns:
-              return False
+        for f in [f for rt in stmt.targetList for f in rt.val.fields]:
+            if f.sval not in columns:
+                return False
 
     # validate from clause to see if they are valid tables
     if stmt.fromClause is None:
-      for rv in stmt.fromClause:
-          if rv.relname not in columns_by_table_dict:
-              return False
+        for rv in stmt.fromClause:
+            if rv.relname not in columns_by_table_dict:
+                return False
 
     # validate where clause to see if they are valid columns
     if not _is_valid_where_clause(stmt.whereClause, columns):
@@ -25,6 +27,7 @@ def validate_select(stmt, columns_by_table_dict):
 
     return True
 
+
 def validate_insert(stmt, columns_by_table_dict):
     if stmt.relation is not None:
         table_name = stmt.relation.relname
@@ -34,6 +37,7 @@ def validate_insert(stmt, columns_by_table_dict):
             if rt.name not in columns_by_table_dict[table_name]:
                 return False
     return True
+
 
 def validate_update(stmt, columns_by_table_dict):
     if stmt.relation is not None:
@@ -46,10 +50,12 @@ def validate_update(stmt, columns_by_table_dict):
                 return False
     # Checking WHERE clause
     # todo: improve this later. for now just check is a valid column in the schema. Then we can check if the column is valid for the table, but for that we would need to dig deeper in the ast
-    columns = [col for table in columns_by_table_dict for col in columns_by_table_dict[table]]
+    columns = [
+        col for table in columns_by_table_dict for col in columns_by_table_dict[table]]
     if not _is_valid_where_clause(stmt.whereClause, columns):
         return False
     return True
+
 
 def validate_delete(stmt, columns_by_table_dict):
     if stmt.relation is not None:
@@ -58,10 +64,12 @@ def validate_delete(stmt, columns_by_table_dict):
             return False
     # Checking WHERE clause
     # todo: improve this later. for now just check is a valid column in the schema. Then we can check if the column is valid for the table, but for that we would need to dig deeper in the ast
-    columns = [col for table in columns_by_table_dict for col in columns_by_table_dict[table]]
+    columns = [
+        col for table in columns_by_table_dict for col in columns_by_table_dict[table]]
     if not _is_valid_where_clause(stmt.whereClause, columns):
         return False
     return True
+
 
 def _is_valid_where_clause(clause, columns):
     if clause is not None and lexpr in clause and isinstance(clause.lexpr, ast.ColumnRef) and clause.lexpr.fields[0].sval not in columns:
