ce17de850cab020102399d42ffe2df799c0f45c6
depombo <lfdepombo@gmail.com>
2023-05-01 20:13:19 -0400

    only show collapsible menu if it has content

--- dashboard/src/components/Navbar.tsx
+++ dashboard/src/components/Navbar.tsx
@@ -28,14 +28,18 @@ export default function Navbar({ userPic }: { userPic: string }) {
             <div className='relative flex items-center justify-between h-16'>
               <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
                 {/* Mobile menu button*/}
-                <Disclosure.Button className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white focus:outline-none opacity-90 hover:opacity-100'>
-                  <span className='sr-only'>Open main menu</span>
-                  {open ? (
-                    <XIcon className='block h-6 w-6' aria-hidden='true' />
-                  ) : (
-                    <MenuIcon className='block h-6 w-6' aria-hidden='true' />
-                  )}
-                </Disclosure.Button>
+                {
+                  navigation.length > 0 && (
+                    <Disclosure.Button className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white focus:outline-none opacity-90 hover:opacity-100'>
+                      <span className='sr-only'>Open main menu</span>
+                      {open ? (
+                        <XIcon className='block h-6 w-6' aria-hidden='true' />
+                      ) : (
+                        <MenuIcon className='block h-6 w-6' aria-hidden='true' />
+                      )}
+                  </Disclosure.Button>
+                  )
+                }
               </div>
               <div className='flex-1 flex items-center justify-center sm:items-stretch sm:justify-start'>
                 <a href={homeUrl} className='flex-shrink-0 flex items-center mr-6'>
