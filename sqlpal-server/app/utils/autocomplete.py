4af2aa91236b6b3102a464c00e75713a7633e39e
Yolanda <info@ysoft.biz>
2023-05-04 14:35:32 +0200

    feat: generate sample queries for each table on discover
    Closes: #66

--- sqlpal-server/app/utils/autocomplete.py
+++ sqlpal-server/app/utils/autocomplete.py
@@ -40,6 +40,28 @@ Please autocomplete the following SQL fragment: {query}
 
 """)
 
+SAMPLE_QUERIES_TEMPLATE = os.environ.get('SAMPLE_QUERIES_PROMPT', """
+You are an SQL assistant, capable of generating valid SQL queries. Your goal is to read data from a table and generate relevant example queries that can be used for testing.
+The queries need to cover the whole range of SQL language, including SELECT, INSERT, UPDATE and DELETE, using relevant columns from the table.
+Use the following premises:
+- write a syntactically correct query using {dialect}
+- generate queries with real examples, not using placeholders
+- end your query with a semicolon
+
+Generate a list of 15 queries based on this table:
+
+{table_info}
+
+The output needs to be just a JSON list with this format:
+[
+"query1",
+"query2",
+"query3"
+]
+
+Only provide this list without any additional output.
+""")
+
 
 def predict(llm, query, docsearch):
     # different search types
@@ -67,7 +89,7 @@ def predict(llm, query, docsearch):
     prompt = PromptTemplate(
         input_variables=["query", "table_info", "dialect"], template=CUSTOM_TEMPLATE)
     llm_chain = LLMChain(llm=llm, prompt=prompt)
-    res = llm_chain.predict(table_info=docs, query=query, dialect='Postgres')
+    res = llm_chain.predict(table_info=docs, query=query, dialect='PostgreSQL')
     logger.info("Result from LLM: "+res)
 
     return res
@@ -133,7 +155,7 @@ def autocomplete_chat(query, docsearch):
 
 def autocomplete_openai(query, docsearch):
     llm = OpenAI(temperature=os.environ.get('TEMPERATURE', 0.9),
-                 model_name=os.environ.get('LLM_MODEL', 'gpt-3.5-turbo'), n=int(os.environ.get('OPENAI_NUM_ANSWERS', 1)))
+                 model_name=os.environ.get('LLM_MODEL', 'text-davinci-002'), n=int(os.environ.get('OPENAI_NUM_ANSWERS', 1)))
     res = predict(llm, query, docsearch)
     final_queries = extract_queries_from_result(res)
     return final_queries
@@ -150,7 +172,7 @@ def autocomplete_selfhosted(query, docsearch):
 
     prompt = PromptTemplate(
         input_variables=["query", "table_info", "dialect"], template=CUSTOM_TEMPLATE)
-    query = prompt.format(query=query, table_info=docs, dialect='Postgres')
+    query = prompt.format(query=query, table_info=docs, dialect='PostgreSQL')
 
     # issue a request to an external API
     request = {
@@ -223,3 +245,104 @@ def autocomplete_query(query, docsearch, columns_by_table_dict):
         except Exception as e:
             logger.exception(e)
     return final_query
+
+
+def predict_queries(llm, schema):
+    prompt = PromptTemplate(
+        input_variables=["table_info", "dialect"], template=SAMPLE_QUERIES_TEMPLATE)
+    llm_chain = LLMChain(llm=llm, prompt=prompt)
+    res = llm_chain.predict(dialect='PostgreSQL', table_info=schema)
+    logger.info("Result from LLM: "+res)
+
+    return res
+
+
+def queries_chat(schema):
+    llm = ChatOpenAI(temperature=os.environ.get('TEMPERATURE', 0.9),
+                     model_name=os.environ.get('LLM_QUERIES_MODEL', 'gpt-3.5-turbo'), n=1)
+    res = predict_queries(llm, schema)
+    final_queries = extract_queries_from_result(res)
+    return final_queries
+
+
+def queries_openai(schema):
+    llm = OpenAI(temperature=os.environ.get('TEMPERATURE', 0.9),
+                 model_name=os.environ.get('LLM_QUERIES_MODEL', 'text-davinci-002'), n=1)
+    res = predict_queries(llm, schema)
+    final_queries = extract_queries_from_result(res)
+    return final_queries
+
+
+def queries_selfhosted(schema):
+    prompt = PromptTemplate(
+        input_variables=["table_info", "dialect"], template=SAMPLE_QUERIES_TEMPLATE)
+    query = prompt.format(table_info=schema, dialect='PostgreSQL')
+
+    # issue a request to an external API
+    request = {
+        'prompt': query,
+        'temperature': float(os.environ.get('TEMPERATURE', 1.3)),
+        'top_p': 0.1,
+        'typical_p': 1,
+        'repetition_penalty': 1.18,
+        'top_k': 40,
+        'min_length': 0,
+        'no_repeat_ngram_size': 0,
+        'num_beams': 1,
+        'penalty_alpha': 0,
+        'length_penalty': 1,
+        'early_stopping': False,
+        'seed': -1,
+        'add_bos_token': True,
+        'truncation_length': 2048,
+        'ban_eos_token': False,
+        'skip_special_tokens': True,
+        'stopping_strings': []
+    }
+
+    try:
+        response = requests.post(os.environ.get('LLM_HOST', ''), json=request, auth=HTTPBasicAuth(
+            os.environ.get('LLM_USER', ''), os.environ.get('LLM_PASSWORD', '')))
+        if response.status_code == 200:
+            result = response.json()['results'][0]['text']
+            logger.info("Result from LLM: "+result)
+            final_queries = extract_queries_from_result(result)
+            return final_queries
+    except Exception as e:
+        logger.exception("Error in autocomplete_selfhosted: "+e)
+
+    return None
+
+
+def generate_queries_for_schema(schema, columns_by_table_dict):
+    if os.environ.get('QUERIES_METHOD', 'chat') == 'chat':
+        queries = queries_chat(schema)
+    elif os.environ.get('QUERIES_METHOD', 'chat') == 'openai':
+        queries = queries_openai(schema)
+    elif os.environ.get('QUERIES_METHOD', 'chat') == 'selfhosted':
+        queries = queries_selfhosted(schema)
+    else:
+        queries = queries_chat(schema)
+
+    # validate all queries and return only the accepted ones
+    final_queries = []
+    for q in queries:
+        try:
+            parsed_query_stmt = parse_sql(q.replace('"', ''))
+            is_valid = True
+            for s in parsed_query_stmt:
+                stmt = s.stmt
+                if isinstance(stmt, ast.SelectStmt):
+                    is_valid = validate_select(stmt, columns_by_table_dict)
+                elif isinstance(stmt, ast.InsertStmt):
+                    is_valid = validate_insert(stmt, columns_by_table_dict)
+                elif isinstance(stmt, ast.UpdateStmt):
+                    is_valid = validate_update(stmt, columns_by_table_dict)
+                elif isinstance(stmt, ast.DeleteStmt):
+                    is_valid = validate_delete(stmt, columns_by_table_dict)
+            if is_valid:
+                final_queries.append(q)
+
+        except Exception as e:
+            logger.exception(e)
+    return final_queries
