f74af148ed8ecf8706b927a4223304d42ae15e96
Alejandro Guillen <aguillenv93@gmail.com>
2023-05-04 14:05:21 +0200

    dry a bit

--- dashboard/src/pages/api/sqlParse.ts
+++ dashboard/src/pages/api/sqlParse.ts
@@ -8,10 +8,15 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { parse } from 'pgsql-parser';
 import { inspect } from 'util';
 
+type Schema = {
+  [tableName: string]: { [columnName: string]: { dataType: string; isMandatory: boolean } } & {
+    recordCount: number;
+  };
+};
+
 async function sqlParse(req: NextApiRequest, res: NextApiResponse) {
   console.log('Handling request', {
     app: 'parse',
-    env: process.env.IASQL_ENV,
     meta: req.body,
   });
   const t1 = Date.now();
@@ -25,12 +30,22 @@ async function sqlParse(req: NextApiRequest, res: NextApiResponse) {
   }
   console.log('Parsed content', {
     app: 'parse',
-    env: process.env.IASQL_ENV,
     meta: parsedContent,
   });
   // validate parsed content
   const rawStmt = parsedContent[0].RawStmt as RawStmt;
-  const validationErr: string = validateStatement(rawStmt, schema);
+  let validationErr: string = '';
+  try {
+    validationErr = validateStatement(rawStmt, schema);
+  } catch (e: any) {
+    console.error('Error validating statement', {
+      app: 'parse',
+      meta: {
+        error: e?.message ?? 'Unknown error',
+        rawStmt: inspect(rawStmt, { depth: 4 }),
+      },
+    });
+  }
   const t2 = Date.now();
   console.log(`Total runtime took ${t2 - t1}`, {
     app: 'parse',
@@ -45,104 +60,95 @@ async function sqlParse(req: NextApiRequest, res: NextApiResponse) {
 
 export default sqlParse;
 
-function validateStatement(rawStmt: RawStmt, schema: any): string {
-  let res = '';
+function validateStatement(rawStmt: RawStmt, schema: Schema): string {
+  let err = '';
   // validate select statement
-  if (Object.getOwnPropertyNames(rawStmt.stmt).find(k => k === 'SelectStmt')) {
-    try {
-      res = validateSelectStmt((rawStmt.stmt as OneOfSelectStmt).SelectStmt, schema);
-    } catch (e: any) {
-      console.error('Error validating select statement', {
-        app: 'parse',
-        meta: {
-          error: e?.message ?? 'Unknown error',
-          rawStmt: JSON.stringify(rawStmt, null, 2),
-        },
-      });
-    }
+  const selectStmt = extractSelectStmt(rawStmt.stmt);
+  if (selectStmt) {
+    err = validateSelectStmt(selectStmt, schema);
   }
   // todo: validate insert statement
   // todo: validate update statement
   // todo: validate delete statement
-  return res;
+  return err;
 }
 
-function validateSelectStmt(
-  selectStmt: SelectStmt,
-  schema: {
-    [tableName: string]: { [columnName: string]: { dataType: string; isMandatory: boolean } } & {
-      recordCount: number;
-    };
-  },
-): string {
+function validateSelectStmt(selectStmt: SelectStmt, schema: Schema): string {
   let err = '';
-  console.log(`select statement ${inspect(selectStmt, { depth: 5 })}`);
-  console.log(`schema ${inspect(schema, { depth: 5 })}`);
   const schemaColumns = Object.values(schema)
     .map(t => Object.keys(t))
     .flat();
-  console.log(`schema columns ${inspect(schemaColumns, { depth: 5 })}`);
-  console.log(`target list ${inspect(selectStmt.targetList, { depth: 5 })}`);
   const resTargetVals = selectStmt.targetList?.map(t => t.ResTarget?.val);
   const columnNames: string[] = [];
-  console.log(`column names ${inspect(resTargetVals, { depth: 5 })}`);
   for (const resTarget of resTargetVals ?? []) {
-    if (Object.getOwnPropertyNames(resTarget).find(k => k === 'ColumnRef')) {
-      const columnRef = (resTarget as OneOfColumnRef).ColumnRef;
-      const columnField = columnRef.fields[0];
-      if (Object.getOwnPropertyNames(columnField).find(k => k === 'String')) {
-        const columnName = (columnField as OneOfString).String.str;
-        columnNames.push(columnName);
-        if (!schemaColumns.includes(columnName ?? '')) {
-          return `Column "${columnName}" does not exist in schema`;
-        }
-      }
+    const columnName = extractColumnNameIfColumnRef(resTarget);
+    if (!!columnName && !schemaColumns.includes(columnName ?? '')) {
+      return `Column "${columnName}" does not exist in schema`;
+    } else if (!!columnName) {
+      columnNames.push(columnName);
     }
   }
   const fromClause = selectStmt.fromClause?.[0];
   if (fromClause) {
-    if (Object.getOwnPropertyNames(fromClause).find(k => k === 'RangeVar')) {
-      const relName = (fromClause as OneOfRangeVar).RangeVar.relname;
-      if (!schema[relName]) {
-        return `Table "${relName}" does not exist in schema`;
-      } else {
-        const columns = Object.keys(schema[relName]);
-        for (const columnName of columnNames ?? []) {
-          if (!columns.includes(columnName ?? '')) {
-            return `Column "${columnName}" does not exist in table "${relName}"`;
-          }
+    const relName = extractRelNameIfRangeVar(fromClause);
+    if (relName && !schema[relName]) {
+      return `Table "${relName}" does not exist in schema`;
+    } else if (relName) {
+      const columns = Object.keys(schema[relName]);
+      for (const columnName of columnNames ?? []) {
+        if (!columns.includes(columnName ?? '')) {
+          return `Column "${columnName}" does not exist in table "${relName}"`;
         }
       }
     }
   }
   const whereClause = selectStmt.whereClause;
   if (whereClause) {
-    console.log(`where clause ${inspect(whereClause, { depth: 5 })}`);
-    if (Object.getOwnPropertyNames(whereClause).find(k => k === 'A_Expr')) {
-      const aExpr = (whereClause as OneOfA_Expr).A_Expr as A_Expr;
+    const aExpr = extractAExpr(whereClause);
+    if (aExpr) {
       const left = aExpr.lexpr;
-      const right = aExpr.rexpr;
-      if (Object.getOwnPropertyNames(left).find(k => k === 'ColumnRef')) {
-        const columnRef = (left as OneOfColumnRef).ColumnRef;
-        const columnField = columnRef.fields[0];
-        if (Object.getOwnPropertyNames(columnField).find(k => k === 'String')) {
-          const columnName = (columnField as OneOfString).String.str;
-          if (!schemaColumns.includes(columnName)) {
-            return `Column "${columnName}" does not exist in schema`;
-          }
-        }
+      const leftColumnName = extractColumnNameIfColumnRef(left);
+      if (!!leftColumnName && !schemaColumns.includes(leftColumnName)) {
+        return `Column "${leftColumnName}" does not exist in schema`;
       }
-      if (Object.getOwnPropertyNames(right).find(k => k === 'ColumnRef')) {
-        const columnRef = (right as OneOfColumnRef).ColumnRef;
-        const columnField = columnRef.fields[0];
-        if (Object.getOwnPropertyNames(columnField).find(k => k === 'String')) {
-          const columnName = (columnField as OneOfString).String.str;
-          if (!schemaColumns.includes(columnName)) {
-            return `Column "${columnName}" does not exist in schema`;
-          }
-        }
+      const right = aExpr.rexpr;
+      const rightColumnName = extractColumnNameIfColumnRef(right);
+      if (!!rightColumnName && !schemaColumns.includes(rightColumnName)) {
+        return `Column "${rightColumnName}" does not exist in schema`;
       }
     }
   }
   return err;
+}
+
+function extractSelectStmt(obj: any): SelectStmt | undefined {
+  if (Object.getOwnPropertyNames(obj).find(k => k === 'SelectStmt')) {
+    return (obj as OneOfSelectStmt).SelectStmt as SelectStmt;
+  }
+  return undefined;
+}
+
+function extractAExpr(obj: any): A_Expr | undefined {
+  if (Object.getOwnPropertyNames(obj).find(k => k === 'A_Expr')) {
+    return (obj as OneOfA_Expr).A_Expr as A_Expr;
+  }
+  return undefined;
+}
+
+function extractRelNameIfRangeVar(obj: any): string | undefined {
+  if (Object.getOwnPropertyNames(obj).find(k => k === 'RangeVar')) {
+    return (obj as OneOfRangeVar).RangeVar.relname;
+  }
+  return undefined;
+}
+
+function extractColumnNameIfColumnRef(obj: any): string | undefined {
+  if (Object.getOwnPropertyNames(obj).find(k => k === 'ColumnRef')) {
+    const columnRef = (obj as OneOfColumnRef).ColumnRef;
+    const columnField = columnRef.fields[0];
+    if (Object.getOwnPropertyNames(columnField).find(k => k === 'String')) {
+      return (columnField as OneOfString).String.str;
+    }
+  }
+  return undefined;
 }
