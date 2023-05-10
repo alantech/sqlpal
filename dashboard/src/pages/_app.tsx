de3e49f23dab108c7a01341e48c273ef7a8dd71e
Alejandro Guillen <aguillenv93@gmail.com>
2023-05-10 12:59:27 +0200

    Update app desc

--- dashboard/src/pages/_app.tsx
+++ dashboard/src/pages/_app.tsx
@@ -16,8 +16,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta name='theme-color' content='#000000' />
-        <meta name='description' content='Cloud infrastructure as a PostgreSQL DB' />
-        <title>IaSQL Dashboard</title>
+        <meta name='description' content='Your pal for SQL, of course using LLM' />
+        <title>SQLPal</title>
       </Head>
       <AppConfigProvider>
         <Component {...pageProps} />
