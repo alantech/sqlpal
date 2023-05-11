7c07d0b1a088b87772802dedd4e56c912165d038
Alejandro Guillen <aguillenv93@gmail.com>
2023-05-11 20:27:14 +0200

    send request from server

--- sqlpal-server/app/utils/autocomplete.py
+++ sqlpal-server/app/utils/autocomplete.py
@@ -11,7 +11,6 @@ import os
 from langchain.chains.chat_vector_db.prompts import QA_PROMPT
 import logging
 from difflib import SequenceMatcher
-from json import loads
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
@@ -279,7 +278,7 @@ def generate_queries_for_schema(schema, schema_dict, dialect):
     final_queries = []
     for q in queries:
         try:
-            validation_err = validate_query(q, schema_dict)
+            validation_err = validate_query(q, schema_dict, dialect)
             if validation_err is not None:
                 logger.info("Query {} not valid: {}".format(q, validation_err))
                 continue
@@ -290,9 +289,9 @@ def generate_queries_for_schema(schema, schema_dict, dialect):
     return final_queries
 
 
-def validate_query(query, schema_dict):
+def validate_query(query, schema_dict, dialect):
     try:
-        request = {'content': query, 'schema': schema_dict}
+        request = {'content': query, 'schema': schema_dict, 'dialect': dialect, 'fromServer': True }
         response = requests.post(
             'http://localhost:9876/api/sqlParser/validate', json=request,)
         if response.status_code == 400:
