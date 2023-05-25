ad1c542117e4a45f7551e06b31de5757beef3426
Alejandro Guillen <aguillenv93@gmail.com>
2023-05-25 16:43:28 +0200

    use env

--- sqlpal-editor/renderer/pages/api/config.ts
+++ sqlpal-editor/renderer/pages/api/config.ts
@@ -2,7 +2,7 @@ import { NextApiResponse } from 'next';
 
 const handler = ({} = {}, res: NextApiResponse) => {
   res.status(200).json({
-    iasqlEnv: 'ci', //process.env.IASQL_ENV,
+    iasqlEnv: process.env.IASQL_ENV,
     uid: 'uid', //process.env.IASQL_UID,
     telemetry: process.env.IASQL_TELEMETRY,
   });
