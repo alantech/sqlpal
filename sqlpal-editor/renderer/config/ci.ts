591c0a0e62e17b3c584f7c8b5a186b094fddb271
Alejandro Guillen <aguillenv93@gmail.com>
2023-05-25 16:43:22 +0200

    add auth config

--- sqlpal-editor/renderer/config/ci.ts
+++ sqlpal-editor/renderer/config/ci.ts
@@ -8,6 +8,16 @@ const ci: ConfigInterface = {
     backendUrl: '/api/run',
     serverUrl: '/api/server',
   },
+  auth: {
+    domain: 'sqlpal.us.auth0.com',
+    clientId: 'gFqVK6oPKmfGrir2j3YbLMHiROSgkSuV',
+    authorizationParams: {
+      redirect_uri: 'http://localhost/callback',
+      scope: 'read:current_user',
+      audience: 'https://api.sqlpal.ai',
+    },
+    useRefreshTokens: true,
+  },
 };
 
 export default ci;
--- sqlpal-editor/renderer/config/local.ts
+++ sqlpal-editor/renderer/config/local.ts
@@ -2,7 +2,7 @@ import posthog from 'posthog-js';
 
 import { ConfigInterface } from './config';
 
-const local: ConfigInterface = {
+const local: ConfigInterface = !!global.window ? {
   name: 'local',
   engine: {
     pgHost: 'localhost',
@@ -10,17 +10,27 @@ const local: ConfigInterface = {
     backendUrl: '/api/run',
     serverUrl: 'api/server',
   },
-  posthog: {
-    key: 'phc_xvAQWfpHug7G0SuU5P9wwAbvP9ZawgAfIEZ9FUsiarS',
+  // posthog: {
+  //   key: 'phc_xvAQWfpHug7G0SuU5P9wwAbvP9ZawgAfIEZ9FUsiarS',
+  // },
+  // sentry: {
+  //   dsn: 'https://8ba9a3820f7f4179b5dc12754da9c943@o1090662.ingest.sentry.io/6544238',
+  //   integrations: [new posthog.SentryIntegration(posthog, 'iasql', 6544238)],
+  //   environment: 'local',
+  // },
+  // logdna: {
+  //   key: 'b98181227b606d8ee6c5674b5bb948e7',
+  // },
+  auth: {
+    domain: 'sqlpal.us.auth0.com',
+    clientId: 'gFqVK6oPKmfGrir2j3YbLMHiROSgkSuV',
+    authorizationParams: {
+      redirect_uri: 'http://localhost/callback',
+      scope: 'read:current_user',
+      audience: 'https://api.sqlpal.ai',
+    },
+    useRefreshTokens: true,
   },
-  sentry: {
-    dsn: 'https://8ba9a3820f7f4179b5dc12754da9c943@o1090662.ingest.sentry.io/6544238',
-    integrations: [new posthog.SentryIntegration(posthog, 'iasql', 6544238)],
-    environment: 'local',
-  },
-  logdna: {
-    key: 'b98181227b606d8ee6c5674b5bb948e7',
-  },
-};
+  } : ({} as ConfigInterface);
 
 export default local;
