6c6f59efb44dae314e42627a3b78b54a5fee7452
Alejandro Guillen <aguillenv93@gmail.com>
2023-06-02 16:43:07 +0200

    temporally comment config

--- sqlpal-editor/renderer/config/prod.ts
+++ sqlpal-editor/renderer/config/prod.ts
@@ -10,9 +10,9 @@ const prod: ConfigInterface = !!global.window ? {
     backendUrl: '/api/run',
     serverUrl: 'api/server',
   },
-  posthog: {
-    key: 'phc_8AIyTQ8yfM5hEeUEv4gMmIqIHq0KJ3q6nszlZKTmnDw',
-  },
+  // posthog: {
+  //   key: 'phc_8AIyTQ8yfM5hEeUEv4gMmIqIHq0KJ3q6nszlZKTmnDw',
+  // },
   // sentry: {
   //   dsn: 'https://8ba9a3820f7f4179b5dc12754da9c943@o1090662.ingest.sentry.io/6544238',
   //   integrations: [new posthog.SentryIntegration(posthog, 'iasql', 6544238)],
@@ -21,16 +21,16 @@ const prod: ConfigInterface = !!global.window ? {
   // logdna: {
   //   key: 'b98181227b606d8ee6c5674b5bb948e7',
   // },
-  auth: {
-    domain: 'sqlpal.us.auth0.com',
-    clientId: 'gFqVK6oPKmfGrir2j3YbLMHiROSgkSuV',
-    authorizationParams: {
-      redirect_uri: 'http://localhost/callback',
-      scope: 'read:current_user',
-      audience: 'https://api.sqlpal.ai',
-    },
-    useRefreshTokens: true,
-  },
+  // auth: {
+  //   domain: 'sqlpal.us.auth0.com',
+  //   clientId: 'gFqVK6oPKmfGrir2j3YbLMHiROSgkSuV',
+  //   authorizationParams: {
+  //     redirect_uri: 'http://localhost/callback',
+  //     scope: 'read:current_user',
+  //     audience: 'https://api.sqlpal.ai',
+  //   },
+  //   useRefreshTokens: true,
+  // },
   } : ({} as ConfigInterface);
 
 export default prod;
