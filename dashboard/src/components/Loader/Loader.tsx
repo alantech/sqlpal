df12c81185de2a7c54aeb42187e79915b394dd6d
Alejandro Guillen <aguillenv93@gmail.com>
2023-06-06 13:36:27 +0200

    remove dashboard

--- dashboard/src/components/Loader/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Loader.module.css';
-
-export default function Loader() {
-  return (
-    <div className={`${styles['loader-wrapper']}`}>
-      <div className={`${styles['loader-logo']}`} />
-    </div>
-  );
-}
--- dashboard/src/hooks/useIsomorphicLayoutEffect.ts
+++
@@ -1,7 +0,0 @@
-// https://usehooks-ts.com/react-hook/use-isomorphic-layout-effect
-import { useLayoutEffect, useEffect } from 'react'
-
-const useIsomorphicLayoutEffect =
-  typeof window !== 'undefined' ? useLayoutEffect : useEffect
-
-export default useIsomorphicLayoutEffect;
